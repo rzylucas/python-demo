4cb0f173241925cb3b616814e277211a37d144be
rzy <rzylucas@163.com>
2020-07-14 15:17:28 +0800

    rewrite

--- example/basic/demo1.py
+++ example/basic/demo1.py
@@ -10,14 +10,7 @@
 
 list = [11, 22, 33, 44, 55, 66, 77, 88, 99, 90]
 tup = {
-    'k1':[],
-    'k2':[]
+    'k1':[x for x in list if x>66],
+    'k2':[x for x in list if x<66]
 }
-
-for i in list:
-    if i > 66:
-        tup['k1'].append(i)
-    else:
-        tup['k2'].append(i)
-
 print(tup)
